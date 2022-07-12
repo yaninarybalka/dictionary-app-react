c1303c633a31279ba66fc4e7bfd6491be6ce728f
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-12 17:17:17 +0300

    set a condition to render only 3 definitions

--- src/Meaning.js
+++ src/Meaning.js
@@ -7,16 +7,20 @@ export default function Meaning(props) {
     <div>
       <h4>{props.data.partOfSpeech}</h4>
       {props.data.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <strong>Definition: </strong>
-              {definition.definition} <br />
-              <strong>Example: </strong>
-              {definition.example}
-            </p>
-          </div>
-        );
+        if (index < 3) {
+          return (
+            <div key={index}>
+              <p>
+                <strong>Definition: </strong>
+                {definition.definition} <br />
+                <strong>Example: </strong>
+                {definition.example}
+              </p>
+            </div>
+          );
+        } else {
+          return null;
+        }
       })}
     </div>
   );
