c7072de6425fef860585252a56b88cd8fc296a6f
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-12 12:05:57 +0300

    imported Bootstrap, removed demo code and added an img to App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import developer from "./developer.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={developer} className="App-logo w-50" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
