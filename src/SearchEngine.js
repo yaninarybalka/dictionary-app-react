0bd1667ee6d5da5dde7b20fd940a54c170c58dc5
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-12 15:17:56 +0300

    added search engine that updates when a new word is searched

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,13 +1,33 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./SearchEngine.css";
 import developer from "./developer.jpg";
 
 export default function SearchEngine() {
+  let [searchInput, setSearchInput] = useState(null);
+
+  function handleSearch(event) {
+    setSearchInput(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(searchInput);
+  }
+
   return (
     <div className="Search-engine">
-      <header className="search-engine">
-        <img src={developer} className="App-logo w-50" alt="logo" />
-      </header>
+      <div className="text-wrapp">
+        <h1 className="title"> Dictionary</h1>
+        <h2 className="description"> Choose a word and look it up!</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Type a word"
+            onChange={handleSearch}
+          />
+          <input type="submit" value="GO" />
+        </form>
+        <h5 className="example">e.g. wine, sea, mermaid, forest</h5>
+      </div>
     </div>
   );
 }
