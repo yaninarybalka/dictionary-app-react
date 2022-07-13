da342e269c099b488dfb75484644dcd55d59ec58
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-13 13:36:42 +0300

    rendered audio component

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-export default function Phonetics() {
-  
-  return <div>Hello from phonetics</div>;
+export default function Phonetics(props) {
+  console.log(props.data);
+  if (props.data[0].audio) {
+    return (
+      <div>
+        {props.data[0].text}
+        <audio controls src={props.data[0].audio}>
+          🔊
+        </audio>
+      </div>
+    );
+  } else {
+    return <div>{props.data[0].text}</div>;
+  }
 }
--- src/Result.js
+++ src/Result.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Synonyms from "./Synonyms";
+import Phonetics from "./Phonetics";
 
 export default function Result(props) {
+  console.log(props.data);
   if (props.data) {
     return (
       <div className="results">
         <h4>{props.data.word}</h4>
+        <Phonetics data={props.data.phonetics} />
         <Synonyms data={props.data.meanings[0]} />
+
         {props.data.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
