cd3da45cfcafe604cbfe01247129cb288d542bf3
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-13 22:20:12 +0300

    added rel noreferrer

--- src/index.js
+++ src/index.js
@@ -11,17 +11,28 @@ root.render(
       <SearchEngine />
       <footer class="text-center">
         <h7 class=""> This website was coded by Yanina Rybalka </h7> <br />
-        <a href="https://github.com/yaninarybalka" target="_blank" class="icon">
+        <a
+          href="https://github.com/yaninarybalka"
+          target="_blank"
+          class="icon"
+          rel="noreferrer"
+        >
           <i class="fa-brands fa-github me-3"></i>
         </a>
         <a
           href="https://www.linkedin.com/in/yanina-rybalka-371b0b173/"
           target="_blank"
           class="icon"
+          rel="noreferrer"
         >
           <i class="fa-brands fa-linkedin me-3"></i>
         </a>
-        <a href="mailto:rybalka.yanina@gmail.com" target="_blank" class="icon">
+        <a
+          href="mailto:rybalka.yanina@gmail.com"
+          target="_blank"
+          class="icon"
+          rel="noreferrer"
+        >
           <i class="fa-solid fa-envelope"></i>
         </a>
       </footer>
