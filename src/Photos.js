26b349ca7981324fbd88330e966920052d799558
yaninarybalka <rybalka.yanina@gmail.com>
2022-07-18 10:16:49 +0300

    changed the size of the images to landscape

--- src/Photos.js
+++ src/Photos.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Photos(props) {
+  console.log(props.data.photos);
   if (props.data.photos) {
     return (
       <div className="row">
@@ -9,7 +10,7 @@ export default function Photos(props) {
             <div className="col-sm" key={index}>
               <a href={photo.url} target="_blank" rel="noreferrer">
                 <img
-                  src={photo.src.small}
+                  src={photo.src.landscape}
                   className="img-fluid example-img mb-2"
                   alt=""
                 ></img>
